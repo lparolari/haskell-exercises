d25529ac58fc17cc0de66c24ac25ec9cde82131e
lparolari <luca.parolari23@gmail.com>
2019-10-07 22:07:08 +0200

    Add credit card validation exercise

---
+++ credit-card-validation.hs
@@ -0,0 +1,20 @@
+toDigits :: Integer -> [Integer]
+toDigits 0 = []
+toDigits x = toDigits (x `div` 10) ++ [x `mod` 10]
+
+doubleEveryOther :: [Integer] -> [Integer]
+doubleEveryOther x = reverse (doubleEveryOtherInternal (reverse x) False)
+doubleEveryOtherInternal [] _ = []
+doubleEveryOtherInternal (x:xs) False = [x] ++ doubleEveryOtherInternal xs True
+doubleEveryOtherInternal (x:xs) True = [x * 2] ++ doubleEveryOtherInternal xs False
+
+sumDigits :: [Integer] -> Integer
+sumDigits [] = 0
+sumDigits [x] = x
+sumDigits (x:xs) = sumDigits (toDigits x) + sumDigits xs
+
+validate :: Integer -> Bool
+validate x = (sumDigits (doubleEveryOther (toDigits x))) `mod` 10 == 0
+
+test1 = validate 4012888888881881
+test2 = validate 4012888888881882
