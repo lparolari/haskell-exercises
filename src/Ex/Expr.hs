1727e05a663b412c9cc32a416f9ce80de944f624
lparolari <luca.parolari23@gmail.com>
2020-03-02 20:14:04 +0100

    Add type inference exercise on expr

--- src/Ex/Expr.hs
+++ src/Ex/Expr.hs
@@ -19,8 +19,12 @@ module Expr where
         (Var x) >>= f = f x
         (Val n) >>= f = (Val n)
         (Add l r) >>= f = Add (l >>= f) (r >>= f)
-    
+
     main = do
         putStrLn (show (fmap (\x -> 7) (Add (Var 'x') (Val 1))))
         putStrLn (show (pure (\x -> 7) <*> (Add (Var 'x') (Val 1))))
         putStrLn (show ((Var 'x') >>= (\x -> pure 7)))
+
+    -- Exercise 3, test 23/01/2018
+    -- Infer the types of the following function definition. 
+    k (Add x y) g = Add (k x g) (k y g)
